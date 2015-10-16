851a627411eb014a1f8ff468f8c1954eade06d63
Przemysław <przemyslaw.cuper@gmail.com>
2015-10-16 09:47:23 +0200

    Created tooltip mini-project - zad 1

--- warsztat2/zad1.js
+++ warsztat2/zad1.js
@@ -1,3 +1,22 @@
 /**
  * Created by pc on 2015-10-16.
  */
+
+$(function(){
+    var allBoxes = $(".box");
+    var elementText = "<div class='tooltip'></div>";
+    var tooltip = null;
+
+    allBoxes.on("mouseenter", function(event){
+        tooltip = $(elementText);
+        tooltip.text($(this).data("text"));
+        tooltip.addClass($(this).data("position"));
+        tooltip.appendTo($(this));
+    });
+    allBoxes.on("mouseleave", function(event){
+        if(tooltip != null) {
+            tooltip.remove();
+            tooltip = null;
+        }
+    });
+});
